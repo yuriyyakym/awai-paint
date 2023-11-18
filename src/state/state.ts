88afc56e36089960ffdb61157594b20d3eae4294
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-11-19 06:56:36 +0700

    Add toolsConfigsState family

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,17 +1,33 @@
-import { state } from 'awai';
-import { Background, Layer } from '../types';
+import { familyState, state } from 'awai';
+import { Background, Layer, ToolName } from '../types';
+import {
+  DEFAULT_BACKGROUND_CONFIG,
+  DEFAULT_LINE_CONFIG,
+  DEFAULT_PENCIL_CONFIG,
+  DEFAULT_RECTANGLE_CONFIG,
+} from './constants';
 
 const BACKGROUND_LAYER: Background = {
-  name: 'Backgrond',
-  type: 'background',
-  color: '#fff',
+  tool: 'background',
+  config: DEFAULT_BACKGROUND_CONFIG,
 };
 
 export const canvasElementState = state<HTMLCanvasElement | null>(null);
 
 export const toolState = state<'line' | 'rectangle' | 'pencil'>('pencil');
 
-export const toolConfigState = state<{ color: string }>({ color: '#ff0000' });
+export const toolsConfigsFamily = familyState((tool) => {
+  switch (tool as ToolName) {
+    case 'background':
+      return DEFAULT_BACKGROUND_CONFIG;
+    case 'line':
+      return DEFAULT_LINE_CONFIG;
+    case 'pencil':
+      return DEFAULT_PENCIL_CONFIG;
+    case 'rectangle':
+      return DEFAULT_RECTANGLE_CONFIG;
+  }
+});
 
 export const layersState = state<Layer[]>([BACKGROUND_LAYER]);
 
