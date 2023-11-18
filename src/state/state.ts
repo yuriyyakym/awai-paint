8fa37efff837d70eef15929090ff8a6202cc99ba
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-11-18 22:41:54 +0700

    Add basic state nodes

---
+++ src/state/state.ts
@@ -0,0 +1,18 @@
+import { state } from 'awai';
+import { Background, Layer } from '../types';
+
+const BACKGROUND_LAYER: Background = {
+  name: 'Backgrond',
+  type: 'background',
+  color: '#fff',
+};
+
+export const canvasElementState = state<HTMLCanvasElement | null>(null);
+
+export const toolState = state<'line' | 'rectangle' | 'pencil'>('line');
+
+export const toolConfigState = state<{ color: string }>({ color: '#ff0000' });
+
+export const layersState = state<Layer[]>([BACKGROUND_LAYER]);
+
+export const currentLayerState = state<Layer | null>(null);
