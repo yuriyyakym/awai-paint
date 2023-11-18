50ca7966ded6d5949b2881d4a4d9aa3c157125bc
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-11-19 06:53:41 +0700

    Add default configs constants

---
+++ src/state/constants.ts
@@ -0,0 +1,21 @@
+import { BackgroundConfig, CurveConfig, LineConfig, RectangleConfig } from '../types';
+
+export const DEFAULT_BACKGROUND_CONFIG: BackgroundConfig = {
+  color: '#ffffff',
+};
+
+export const DEFAULT_LINE_CONFIG: LineConfig = {
+  color: '#dd0000',
+  lineWidth: 1,
+};
+
+export const DEFAULT_RECTANGLE_CONFIG: RectangleConfig = {
+  borderColor: '#333',
+  borderWidth: 1,
+  color: '#ddffff',
+};
+
+export const DEFAULT_PENCIL_CONFIG: CurveConfig = {
+  color: '#dd0000',
+  lineWidth: 1,
+};
