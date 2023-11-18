55daffd94c52d9d9938a25ad89ce8b209d0c3a74
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-11-18 22:42:25 +0700

    Add getEventPoint util

---
+++ src/state/lib/getEventPoint.ts
@@ -0,0 +1,14 @@
+import type { MouseEvent } from 'react';
+
+import type { Point } from '../../types';
+
+const getEventPoint = (event: MouseEvent<HTMLCanvasElement>): Point => {
+  const { left, top } = event.currentTarget.getBoundingClientRect();
+
+  return {
+    x: event.clientX - left,
+    y: event.clientY - top,
+  };
+};
+
+export default getEventPoint;
