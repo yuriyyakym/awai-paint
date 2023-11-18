f9ac72849e8116a0ba77df145e2dbd71568fb4af
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-11-18 22:12:14 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+import './App.css';
+
+const App: FunctionComponent = () => {
+  return <div className="App">Empty app</div>;
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+import './index.css';
+
+const rootElement = document.getElementById('root')!;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
