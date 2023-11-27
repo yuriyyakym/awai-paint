26e0f657466089c9910572d8d47daeba04cfad67
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-11-28 04:40:27 +0700

    Add draw-start scenario

---
+++ src/state/scenarios/draw-start.ts
@@ -0,0 +1,25 @@
+import { scenario } from 'awai';
+
+import {
+  startDrawing,
+  startDrawingLine,
+  startDrawingPencil,
+  startDrawingRectangle,
+} from '../actions';
+import { toolState } from '../state';
+
+scenario(startDrawing.events.invoked, ({ arguments: [point] }) => {
+  const tool = toolState.get();
+
+  if (tool === 'line') {
+    startDrawingLine(point);
+  }
+
+  if (tool === 'pencil') {
+    startDrawingPencil(point);
+  }
+
+  if (tool === 'rectangle') {
+    startDrawingRectangle(point);
+  }
+});
